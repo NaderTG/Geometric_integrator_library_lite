b61f955c26d1f7d8ec6e0a23b0bf4eac12f071d2
antigsuit <ng1312@ic.ac.uk>
2017-12-20 20:32:24 +0000

    Added copyright

--- src/visualise.h
+++ src/visualise.h
@@ -1,10 +1,21 @@
-//
-//  visualise.h
-//  Vorticity_simple
-//
-//  Created by Nader on 19/12/2017.
-//  Copyright © 2017 Nader Ganaba. All rights reserved.
-//
+/*
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2017 Nader Ganaba
+ * Created by Nader on 19/12/2017.
+ * visualise.h
+ */
 
 #ifndef visualise_h
 #define visualise_h
